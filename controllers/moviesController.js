ca089794ffeef341aeb745023de20d537c68bc00
Alioune Seck <aliouneseck15@gmail.com>
2024-01-23 19:41:51 -0500

    added Movie Router

---
+++ controllers/moviesController.js
@@ -0,0 +1,22 @@
+const Movie = require('../models/movie')
+
+
+/*
+// POST /movies: Accepst Movie Data and Creates A Movie
+router.post('/', movieCtrl.create)
+
+// GET /movies: Returns a list of all movies.
+router.get('/', movieCtrl.index)
+
+// GET /movies/:id: Gets An Individual Movie
+router.get('/:id', movieCtrl.show)
+
+// GET /performers: Returns a list of all performers.
+router.get('/:movieId/performers/:performerId', movieCtrl.addPerformer)
+*/
+
+
+
+
+
+
--- routes/moviesRouter.js
+++ routes/moviesRouter.js
@@ -2,15 +2,17 @@ const express = require('express')
 const router = express.Router()
 const movieCtrl = require('../controllers/moviesController')
 
+// POST /movies: Accepst Movie Data and Creates A Movie
+router.post('/', movieCtrl.create)
 
+// GET /movies: Returns a list of all movies.
+router.get('/', movieCtrl.index)
 
+// GET /movies/:id: Gets An Individual Movie
+router.get('/:id', movieCtrl.show)
 
-
-
-
-
-
-
+// GET /performers: Returns a list of all performers.
+router.get('/:movieId/performers/:performerId', movieCtrl.addPerformer)
 
 
 
